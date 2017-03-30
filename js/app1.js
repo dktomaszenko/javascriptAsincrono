eb66442d2390bc1251e6ed4608392f8094d256ac
dktomaszenko <xjxxwbm9fx>
2017-03-30 12:45:38 +0200

    init

--- js/app1.js
+++ js/app1.js
@@ -1,6 +1,19 @@
-function addNumbers(x,y) {
+function addNumbers(x, y) {
 // Upewniamy się, że dane wejściowe są liczbami.
     if (typeof x !== "number" || typeof y !== "number") {
         throw Error("Nieprawidłowe parametry.");
     }
 }
+
+function addNumbersB(x, y) {
+// Upewniamy się, że dane wejściowe są liczbami.
+    x = Number(x);
+    y = Number(y);
+// Operator + bezpiecznie przeprowadzi dodawanie wartości liczbowych.
+    return x + y;
+}
+addNumbers(21, 21); // 42
+addNumbers(21, "21"); // 42
+
+addNumbersB(21, 21); // 42
+addNumbersB(21, "21"); // 42
---
+++ js/app2.js
@@ -0,0 +1,3 @@
+/**
+ * Created by dktomaszenko on 30/03/2017.
+ */
